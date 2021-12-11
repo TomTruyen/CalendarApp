886d597ff8e136ceace850441c8d635ebfb97f16
TomTruyen <tom.truyen@gmail.com>
2021-12-11 18:44:27 +0100

    Add task item

---
+++ calendar_app/lib/shared/widgets/task_item.dart
@@ -0,0 +1,54 @@
+import 'package:calendar_app/models/task.dart';
+import 'package:calendar_app/screens/task_detail_screen.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class TaskItem extends StatelessWidget {
+  final Task task;
+
+  const TaskItem({Key? key, required this.task}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: task.color,
+          ),
+          padding: const EdgeInsets.all(8),
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(task.title, style: _textStyle()),
+              Text(
+                "${_formatTime(time: task.startTime)} - ${_formatTime(time: task.endTime)}",
+                style: _textStyle(),
+              )
+            ],
+          ),
+        ),
+        onTap: () {
+          Navigator.push(
+            context,
+            CupertinoPageRoute(
+              builder: (_) => TaskDetailScreen(
+                task: task,
+              ),
+            ),
+          );
+        });
+  }
+
+  _formatTime({required DateTime time}) {
+    return DateFormat("hh:mm a").format(time).toString();
+  }
+
+  _textStyle() {
+    return const TextStyle(
+      color: Color(0xFFF0F0F0),
+    );
+  }
+}
