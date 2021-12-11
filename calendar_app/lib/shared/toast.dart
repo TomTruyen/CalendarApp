c9ed0c2265bccc0e9c821df40eeb42718ea9212b
TomTruyen <tom.truyen@gmail.com>
2021-12-11 16:29:23 +0100

    Add Toast class

---
+++ calendar_app/lib/shared/toast.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Toast {
+  static display(BuildContext context, {required String message, Duration duration = const Duration(seconds: 2)}) {
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: Text(message),
+      duration: duration,
+    ));
+  }
+}
