b299459089cfd4b00b7d8b09d4891f33417b0d31
TomTruyen <tom.truyen@gmail.com>
2021-12-11 16:30:04 +0100

    Add tasklist

--- calendar_app/lib/screens/calendar_screen.dart
+++ calendar_app/lib/screens/calendar_screen.dart
@@ -1,4 +1,6 @@
+import 'package:calendar_app/models/task.dart';
 import 'package:calendar_app/screens/add_task_screen.dart';
+import 'package:calendar_app/services/task_service.dart';
 import 'package:calendar_app/shared/themes.dart';
 import 'package:calendar_app/shared/widgets/button.dart';
 import 'package:date_picker_timeline/date_picker_widget.dart'
@@ -16,6 +18,7 @@ class CalendarScreen extends StatefulWidget {
 }
 
 class CalendarScreenState extends State<CalendarScreen> {
+  final TaskService _taskService = TaskService.instance;
   DateTime _selectedDate = DateTime.now();
 
   @override
@@ -25,33 +28,16 @@ class CalendarScreenState extends State<CalendarScreen> {
         mainAxisAlignment: MainAxisAlignment.start,
         children: <Widget>[
           _taskBar(),
-          Container(
-            margin: const EdgeInsets.only(top: 20, left: 20),
-            child: DatePickerTimeline.DatePicker(
-              DateTime.now(),
-              initialSelectedDate: DateTime.now(),
-              selectionColor: Theme.of(context).primaryColor,
-              selectedTextColor: Colors.white,
-              dateTextStyle: GoogleFonts.lato(
-                textStyle: const TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.w600,
-                  color: Colors.grey,
-                ),
-              ),
-              onDateChange: (DateTime date) {
-                setState(() {
-                  _selectedDate = date;
-                });
-              },
-            ),
-          ),
+          _datePicker(),
+          Expanded(
+            child: _taskList(),
+          )
         ],
       ),
     );
   }
 
-  _taskBar() {
+  Widget _taskBar() {
     return Container(
       margin: const EdgeInsets.only(left: 20, right: 20, top: 10),
       child: Row(
@@ -81,6 +67,58 @@ class CalendarScreenState extends State<CalendarScreen> {
           )
         ],
       ),
+    );
+  }
+
+  Widget _datePicker() {
+    return Container(
+      margin: const EdgeInsets.only(top: 20, left: 20),
+      child: DatePickerTimeline.DatePicker(
+        DateTime.now(),
+        initialSelectedDate: DateTime.now(),
+        selectionColor: Theme.of(context).primaryColor,
+        selectedTextColor: Colors.white,
+        dateTextStyle: GoogleFonts.lato(
+          textStyle: const TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.w600,
+            color: Colors.grey,
+          ),
+        ),
+        onDateChange: (DateTime date) {
+          setState(() {
+            _selectedDate = date;
+          });
+        },
+      ),
+    );
+  }
+
+  Widget _taskList() {
+    return FutureBuilder(
+      future: _taskService.getByDate(_selectedDate),
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        if (snapshot.connectionState == ConnectionState.none ||
+            snapshot.data == null) {
+          return Center(
+            child: CircularProgressIndicator(
+              color: Theme.of(context).primaryColor,
+            ),
+          );
+        }
+
+        return ListView.builder(
+          itemCount: snapshot.data.length,
+          itemBuilder: (context, index) {
+            Task task = snapshot.data[index];
+            return Column(
+              children: <Widget>[
+                Text(task.title),
+              ],
+            );
+          },
+        );
+      },
     );
   }
 }
