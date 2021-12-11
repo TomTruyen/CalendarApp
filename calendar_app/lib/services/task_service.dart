39183efd66c6757a0b5d4571fce4f5c2bbbf1f4e
TomTruyen <tom.truyen@gmail.com>
2021-12-11 15:34:31 +0100

    Update TaskService
    update task service to make use of a singleton instance so there is only 1 reference to the database throughout the entire application

--- calendar_app/lib/services/task_service.dart
+++ calendar_app/lib/services/task_service.dart
@@ -1,8 +1,15 @@
+import 'dart:io';
+
 import 'package:calendar_app/models/task.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
 
 class TaskService {
+  static const String _databaseName = "calendar_app.db";
+  static const int _databaseVersion = 1;
   static const String tableTask = "tasks";
+
   static const String columnId = "_id";
   static const String columnTitle = "title";
   static const String columnNote = "note";
@@ -12,34 +19,49 @@ class TaskService {
   static const String columnColor = "color";
   static const String columnCompleted = "completed";
 
-  late Database db;
+  // Singleton class
+  TaskService._privateConstructor();
+  static final TaskService instance = TaskService._privateConstructor();
 
-  Future open(String path, {int databaseVersion = 1}) async {
-    db = await openDatabase(
+  // App-wide reference to database
+  static Database? _database;
+  Future<Database> get database async {
+    return _database ??= await _initDatabase();
+  }
+
+  Future<Database> _initDatabase() async {
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+    String path = join(documentsDirectory.path, _databaseName);
+    return await openDatabase(
       path,
-      version: databaseVersion,
-      onCreate: (Database db, int version) async {
-        String tableQuery = 'CREATE TABLE $tableTask (';
-        tableQuery += ' $columnId INTEGER PRIMARY KEY AUTOINCREMENT';
-        tableQuery += ' $columnTitle TEXT NOT NULL' '$columnNote TEXT';
-        tableQuery += ' $columnDate INTEGER';
-        tableQuery += ' $columnStartTime INTEGER';
-        tableQuery += ' $columnEndTime INTEGER';
-        tableQuery += ' $columnColor INTEGER';
-        tableQuery += ' $columnCompleted INTEGER';
-        tableQuery += ')';
-
-        await db.execute(tableQuery);
-      },
+      version: _databaseVersion,
+      onCreate: _onCreate,
     );
   }
 
+  Future _onCreate(Database db, int version) async {
+    await db.execute('''
+      CREATE TABLE $tableTask (
+        $columnId INTEGER PRIMARY KEY AUTOINCREMENT,
+        $columnTitle TEXT NOT NULL' '$columnNote TEXT,
+        $columnDate INTEGER,
+        $columnStartTime INTEGER,
+        $columnEndTime INTEGER,
+        $columnColor INTEGER
+        $columnCompleted INTEGER
+      )
+    ''');
+  }
+
   Future<Task> insert(Task task) async {
+    Database db = await instance.database;
     task.id = await db.insert(tableTask, task.toMap());
     return task;
   }
 
   Future<List<Task>> getAll() async {
+    Database db = await instance.database;
+
     List<Map<String, Object?>> maps = await db.query(tableTask);
 
     if (maps.isNotEmpty) {
@@ -50,6 +72,8 @@ class TaskService {
   }
 
   Future<Task?> getById(int id) async {
+    Database db = await instance.database;
+
     List<Map<String, Object?>> maps = await db.query(
       tableTask,
       where: '$columnId = ?',
@@ -62,6 +86,8 @@ class TaskService {
   }
 
   Future<int> delete(int id) async {
+    Database db = await instance.database;
+
     return await db.delete(
       tableTask,
       where: '$columnId = ?',
@@ -70,6 +96,8 @@ class TaskService {
   }
 
   Future<int> update(Task task) async {
+    Database db = await instance.database;
+
     return await db.update(
       tableTask,
       task.toMap(),
@@ -78,5 +106,9 @@ class TaskService {
     );
   }
 
-  Future close() async => db.close();
+  Future close() async {
+    Database db = await instance.database;
+
+    db.close();
+  }
 }
